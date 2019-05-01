a12a30cf3a81c38e279d29762e47c2cb8c78475c
tjde <tjde@protonmail.com>
2019-05-01 17:15:27 +1000

    Configured AWS storage to display profile picture and updated profile form

--- src/app/controllers/profiles_controller.rb
+++ src/app/controllers/profiles_controller.rb
@@ -70,6 +70,6 @@ class ProfilesController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def profile_params
-      params.require(:profile).permit(:first_name, :last_name, :gender, :age_bracket, :location, :description)
+      params.require(:profile).permit(:first_name, :last_name, :gender, :age_bracket, :location, :picture, :description)
     end
 end
