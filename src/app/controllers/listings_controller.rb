b4b501d8cf2ced3766dd3636651ae113088a3fc8
Ben Crow <benny_crow91@hotmail.com>
2019-05-02 13:01:04 +1000

    listing show changed

--- src/app/controllers/listings_controller.rb
+++ src/app/controllers/listings_controller.rb
@@ -1,5 +1,5 @@
 class ListingsController < ApplicationController
-  before_action :authenticate_user!, only: [:show, :edit, :update, :destroy]
+  before_action :authenticate_user!, only: [:show, :new, :edit, :update, :destroy]
   before_action :set_listing, only: [:show, :edit, :update, :destroy]
 
   # GET /listings
