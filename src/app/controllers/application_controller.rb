d3d597533f9a89bde39c119fb5ffd7a1627450ab
Ben Crow <benny_crow91@hotmail.com>
2019-05-02 17:17:03 +1000

    added authorization features to ensure profile is created and updated nav bar with more logic

--- src/app/controllers/application_controller.rb
+++ src/app/controllers/application_controller.rb
@@ -1,2 +1,8 @@
 class ApplicationController < ActionController::Base
+    private
+    def user_profile_redirect
+        if user_signed_in? and current_user.profile.blank?
+            redirect_to new_profile_path
+        end
+    end
 end
