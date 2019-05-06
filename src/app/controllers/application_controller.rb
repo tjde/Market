36f73e8825189433e894d59917753ad6b18be36c
Ben Crow <benny_crow91@hotmail.com>
2019-05-06 10:33:31 +1000

    Improved search ui by providing a drop down of existing locations

--- src/app/controllers/application_controller.rb
+++ src/app/controllers/application_controller.rb
@@ -2,21 +2,22 @@ class ApplicationController < ActionController::Base
 
     private
 
+    # Using devise's built in method to redirect the user to payment after login/signup
     def after_sign_in_path_for(resource)
         new_payment_path
     end
 
+    # A custom method to prevent the user from trying to create a second profile
     def one_profile
         if user_signed_in? and !current_user.profile.blank?
             redirect_to profile_path(current_user.profile)
         end
     end
-    
+
+    # A customer method to 
     def user_paid
-        if user_signed_in?
-            unless current_user.paid?
-                redirect_to new_payment_path
-            end
+        if user_signed_in? and !current_user.paid?
+            redirect_to new_payment_path
         end
     end
 
--- src/app/models/listing.rb
+++ src/app/models/listing.rb
@@ -9,7 +9,7 @@ class Listing < ApplicationRecord
   def self.search(search, city)
     if search and city
       hobby = Hobby.find_by(name: search)
-      city = City.find_by(name: city)
+      city == [""] ? city = false : city = City.find(city)
       if hobby and city
         self.where(hobby_id: hobby, city_id: city)
       elsif hobby
@@ -19,20 +19,12 @@ class Listing < ApplicationRecord
       else
         Listing.all
       end
-    elsif search
-      hobby = Hobby.find_by(name: search)
-      if hobby
-        self.where(hobby_id: hobby)
-      else
-        Listing.all
-      end
-    elsif city
-      city = City.find_by(name: city)
-      if city
-        self.where(city_id: city)
-      else
-        Listing.all
-      end
+    # elsif search
+    #   hobby = Hobby.find_by(name: search)
+    #   hobby ? self.where(hobby_id: hobby) : Listing.all
+    # elsif city
+    #   city = City.find_by(name: city)
+    #   city ? self.where(city_id: city) : Listing.all
     else
       Listing.all
     end
