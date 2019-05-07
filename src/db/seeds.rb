cced9e6b20d7f9a72ffc65374d0bbc8dccd9a4ab
Ben Crow <benny_crow91@hotmail.com>
2019-05-07 17:13:36 +1000

    Changed the seeds file to setup some users, profiles, a category and some hobbies

--- src/db/seeds.rb
+++ src/db/seeds.rb
@@ -6,15 +6,23 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+
+cities_arr = ['Sydney', 'Melbourne', 'Brisbane', 'Perth', 'Hobart', 'Adelaide']
+cities_arr.each {|city| City.create(name: city)}
 category = Category.create(name: "Sport")
-i = 0
-while i < 11
+
+i = 1
+while i < 6
+    user = User.create(
+        email: "ben+#{i}@test.com",
+        password: "bencrow"
+    )
+    user.paid = true
+    user.save
+    Profile.create(first_name: "ben", last_name: "crow", gender: "male", age_bracket: "18-25", description: "a cool guy", user_id: i, city_id: i)
+    puts "#{i} user and profile created"
     Category.first.hobbies.create(name: Faker::Team.sport)
     puts "created #{i} hobbies"
     i += 1
 end
 
-
-cities_arr = ['Sydney', 'Melbourne', 'Brisbane', 'Perth', 'Hobart', 'Adelaide']
-
-cities_arr.each {|city| City.create(name: city)}
