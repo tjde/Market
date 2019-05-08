44365c89e59c474ba208ba6c25dc6ab34e6d8050
Ben Crow <benny_crow91@hotmail.com>
2019-05-08 11:38:46 +1000

    updated seeds file for testing

--- src/db/seeds.rb
+++ src/db/seeds.rb
@@ -6,15 +6,20 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+cities_arr = ['Sydney', 'Melbourne', 'Brisbane', 'Perth', 'Hobart', 'Adelaide']
+cities_arr.each {|city| City.create(name: city)}
+
 category = Category.create(name: "Sport")
-i = 0
-while i < 11
+i = 1
+while i < 7
+    user = User.new(
+        email: "ben+#{i}@test.com",
+        password: "12345678"
+    )
+    user.paid = true
+    user.save
+    Profile.create(user_id: user.id, first_name: "ben+#{i}", last_name: "crow", gender: "male", age_bracket: "18-26", description: "a test profile", city_id: i)
     Category.first.hobbies.create(name: Faker::Team.sport)
     puts "created #{i} hobbies"
     i += 1
 end
-
-
-cities_arr = ['Sydney', 'Melbourne', 'Brisbane', 'Perth', 'Hobart', 'Adelaide']
-
-cities_arr.each {|city| City.create(name: city)}
