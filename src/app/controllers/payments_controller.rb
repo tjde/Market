578def04f23c41c6942d9daa063f452a7ec9f7a3
tjde <tjde@protonmail.com>
2019-05-12 12:45:37 +1000

    New payments picture and URL code modification

--- src/app/controllers/payments_controller.rb
+++ src/app/controllers/payments_controller.rb
@@ -13,8 +13,8 @@ class PaymentsController < ApplicationController
                 currency: 'aud',
                 quantity: 1,
             }],
-            success_url: 'http://hobbybuddies.herokuapp.com/payments',
-            cancel_url: 'http://hobbybuddies.herokuapp.com/payments'
+            success_url: 'http://hobbybuddies.herokuapp.com/payments/success',
+            cancel_url: 'https://hobbybuddies.herokuapp.com/cancel'
         )
         @stripe_session_id = stripe_session.id
     end
